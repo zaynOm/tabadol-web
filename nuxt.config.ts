c42bf0cf5e20b257236ac10a607858409444e5af
zaynom <ouaziz0626@gmail.com>
2025-01-19 19:53:04 +0100

    feat: add google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  modules: ["@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      Alexandria: [400, 700],
+      Cairo: true,
+    },
+  },
+  css: ["@/assets/css/main.css"],
 });
