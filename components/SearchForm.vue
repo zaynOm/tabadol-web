6270dd6ab24a5dd8d3380707387e7d7e8fdd24fa
zaynom <ouaziz0626@gmail.com>
2025-01-22 10:00:43 +0100

    feat: add basic search form ui

---
+++ components/SearchForm.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { AutoComplete, Button } from "primevue";
+
+const items = ref<string[]>([]);
+
+const state = reactive({
+  currSchool: "",
+});
+
+const search = (event: any) => {
+  items.value = [...Array(10).keys()].map((item) => event.query + "-" + item);
+};
+</script>
+
+<template>
+  <div
+    class="h-[calc(100vh-300px)] flex items-center justify-center p-4 w-full"
+  >
+    <Card class="w-2/4 h-32 justify-center">
+      <template #content>
+        <div class="flex justify-center gap-4">
+          <AutoComplete
+            v-model="state.currSchool"
+            :suggestions="items"
+            @complete="search"
+            placeholder="إسم المدرسة"
+            class="flex-1"
+            input-class="flex-1"
+          />
+          <Button label="بحث" icon="pi pi-search" />
+        </div>
+      </template>
+    </Card>
+  </div>
+</template>
