137ca1db60bc860dc37f3c188e7b790beb912c39
zaynom <ouaziz0626@gmail.com>
2025-01-22 12:14:33 +0100

    feat: conditional rendering signup button or user avatar based on the user auth state

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,25 +1,34 @@
 <script setup lang="ts">
+const { user } = useAuth();
 const isDarkMode = ref(true);
 function toggleDarkMode() {
   document.documentElement.classList.toggle("my-app-dark");
   isDarkMode.value = !isDarkMode.value;
 }
-watch(isDarkMode, () => console.log("moooood", isDarkMode.value));
 </script>
 
 <template>
   <nav class="flex justify-between p-4 h-18">
     <NuxtLink href="/" class="font-bold text-4xl"> تبادل </NuxtLink>
-    <div class="flex gap-4">
+    <div class="flex gap-4 items-center">
       <Button
         :icon="isDarkMode ? 'pi pi-moon' : 'pi pi-sun'"
         aria-label="Toggle dark mode"
         severity="contrast"
         @click="toggleDarkMode()"
       />
-      <NuxtLink href="/login">
+      <NuxtLink href="/login" v-if="!user">
         <Button>سجل الدخول</Button>
       </NuxtLink>
+      <div v-else class="flex items-center">
+        <!-- <span>{{ user.user_metadata.name }}</span> -->
+        <Avatar
+          :image="user.user_metadata.picture"
+          class="mr-2"
+          size="large"
+          shape="circle"
+        />
+      </div>
     </div>
   </nav>
 </template>
